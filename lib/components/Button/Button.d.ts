ab893d9a27e34b0d293cc0927d73c2c0946b15fb
Said Tashmanov <said.tashmanov@fdrive.kz>
2022-11-17 15:17:03 +0600

    Before pushing

---
+++ lib/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+export type ButtonProps = {
+    label: string;
+};
+declare const Button: FC<ButtonProps>;
+export default Button;
---
+++ lib/components/Button/Button.stories.d.ts
@@ -0,0 +1,6 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { ButtonProps } from "./Button";
+declare const _default: Meta<import("@storybook/react/types-6-0").Args>;
+export default _default;
+export declare const Primary: Story<ButtonProps>;
+export declare const Secondary: Story<ButtonProps>;
